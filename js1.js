bf0a43e57d5baa3f44c04a0020288cef5c6c2e45
shabbir <gshabbir10@gmail.com>
2012-10-19 16:08:39 +0500

    re assembla-2

--- js1.js
+++ js1.js
@@ -7,24 +7,7 @@ king objects: 100% (6/6), done.
 From https://github.com/lemon10/lemon
    d2c9a15..7a49e41  master     -> origin/master
 Merge made by recursive.
- TODAY |    1 +
- 1 files changed, 1 insertions(+), 0 deletions(-)
- create mode 100644 TODAY
-shabbir@shabbir:~/www/github/lemon$ git push origin master
-U
-Delta compression using up to 2 threads.
-Compressing objects: 100% (5/5), done.
-Writing objects: 100% (5/5), 16.88 KiB, done.
-Total 5 (delta 2), reused 0 (delta 0)
-To https://github.com/lemon10/lemon.git
-king objects: 100% (6/6), done.
-From https://github.com/lemon10/lemon
-   d2c9a15..7a49e41  master     -> origin/master
-Merge made by recursive.
- TODAY |    1 +
- 1 files changed, 1 insertions(+), 0 deletions(-)
- create mode 100644 TODAY
-shabbir@shabbir:~/www/github/lemon$ git push origin master
+n master
 Username: 
 Password: 
 Counting objects: 7, done.
