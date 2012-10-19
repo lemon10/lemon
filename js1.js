c5491d78381431e8a3cef31b927425dd98784a69
shabbir <gshabbir10@gmail.com>
2012-10-19 16:22:55 +0500

    fix #2

--- js1.js
+++ js1.js
@@ -10,12 +10,6 @@ Merge made by recursive.
 n master
 Username: 
 P
-Writing objects: 100% (5/5), 16.88 KiB, done.
-Total 5 (delta 2), reused 0 (delta 0)
-To https://github.com/lemon10/lemon.git
-king objects
-Password: 
-Counting objects: 7, done.
 Delta compression using up to 2 threads.
 Compressing objects: 100% (5/5), done.
 Writing objects: 100% (5/5), 16.88 KiB, done.
